6163388569f9d2308196df1f1946daf6a4098383
wendydv1989 <wendydv1989@gmail.com>
2022-04-01 04:02:51 +0200

    repost button fix

--- src/pages/home-feed/pages/HomeFeed.jsx
+++ src/pages/home-feed/pages/HomeFeed.jsx
@@ -62,14 +62,11 @@ export function HomeFeed({ homefeed, rePost }) {
 								<div className={main.postLocation}>{moment(item.time).fromNow()}</div>
 							</NavLink>
 						</div>
-						{
-							(item.address === account.address,
-							item.type === 'repost' ? null : (
-								<div className={main.postRepostIcon}>
-									<RepeatRounded onClick={() => rePost(item)}></RepeatRounded>
-								</div>
-							))
-						}
+						{item.address === account.address || item.type === 'repost' ? null : (
+							<div className={main.postRepostIcon}>
+								<RepeatRounded onClick={() => rePost(item)}></RepeatRounded>
+							</div>
+						)}
 					</div>
 
 					{/* <ActionButton type={item.type}></ActionButton> */}
